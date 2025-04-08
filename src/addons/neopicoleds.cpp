59d1c6a6eb5fd9ca8d313184564d9d311c0188e4
Nicholas G <speeedypotato@gmail.com>
2025-04-08 18:59:19 -0500

    NG | SplitFox Pro Config

--- src/addons/neopicoleds.cpp
+++ src/addons/neopicoleds.cpp
@@ -238,7 +238,7 @@ void NeoPicoLEDAddon::setup() {
 
 void NeoPicoLEDAddon::process()
 {
-	AnimationStation::printfs[1] = "Time = " + std::to_string(absolute_time_diff_us(this->lastRunTime, get_absolute_time()) / 1000);
+	// AnimationStation::printfs[1] = "Time = " + std::to_string(absolute_time_diff_us(this->lastRunTime, get_absolute_time()) / 1000);
 
 	if(bRestartLeds)
 	{
